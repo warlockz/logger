6fef5b23592554ae72cab4f1b4eb633c1cdfe118
warlockz <yogerocks@gmail.com>
2017-04-27 06:11:21 +0530

    Initial version of logger completed

--- logger.c
+++ logger.c
@@ -5,19 +5,27 @@
  *      Author: yoge
  */
 
-#include "logger.h"
 
+#include "logger.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
 #include <sys/time.h>
 #include <sys/syscall.h>
 #include <time.h>
 #include <unistd.h>
 
+//http://stackoverflow.com/questions/2124339/c-preprocessor-va-args-number-of-arguments
+//http://www.geeksforgeeks.org/variable-length-arguments-for-macros/
+#define lout(f,x,...) \
+		fprintf(f,x,##__VA_ARGS__); fflush(f);
+
+
 
 FILE* g_File = NULL;
 static logsetting g_log_setting;
 
-int print(const char* format,...)
+int vprint(const char* format,...)
 {
 	va_list myargs;
 	va_start(myargs,format);
@@ -28,10 +36,62 @@ int print(const char* format,...)
 	return 0;
 }
 
+char* varargstostring(const char* format,...)
+{
+	char* msg = (char*)calloc(MAX_EXP_SIZE,1);
+	va_list myargs;
+	va_start(myargs,format);
+	vsnprintf(msg,MAX_EXP_SIZE,format,myargs);
+	va_end(myargs);
+	return msg;
+}
+
+void devlogprint(const char* filename,const char* funname,int lineno,const char* logtype,char* msg)
+{
+	//timespec is POSIX complaint where another struct continued till now
+	//timeval was BSD legacy https://www.gnu.org/software/libc/manual/html_node/Elapsed-Time.html
+	struct timespec monotime;
+	char timestr[MAX_TIME_SIZE];
+	struct tm* calender_cur_time;
+	long millisec;
+	//http://man7.org/linux/man-pages/man2/clock_gettime.2.html
+	memset(&monotime,0,sizeof(monotime));
+	//https://linux.die.net/man/3/clock_gettime
+	clock_gettime(CLOCK_MONOTONIC,&monotime);
+	//http://stackoverflow.com/questions/12392278/measure-time-in-linux-time-vs-clock-vs-getrusage-vs-clock-gettime-vs-gettimeof?noredirect=1&lq=1
+	//convert monotime and get time in struct like tm members
+	calender_cur_time = gmtime(&monotime.tv_sec);
+	//format time
+	strftime(timestr,sizeof(timestr),"%H:%M:%S",calender_cur_time);
+	//convert nano to milli seconds
+	millisec = (monotime.tv_nsec / 1000000);
+
+	if(g_File != NULL)
+	{
+		if(g_log_setting.devmode)
+		{
+			lout(g_File,"%s:%ld [%s] - %s@%s():[%d] :- %s",timestr,millisec,logtype,filename,funname,lineno,msg);
+		}
+		else
+		{
+			lout(g_File,"[%s] - %s :- %s",logtype,funname,msg);
+		}
+	}
+	else if(g_log_setting.devmode == 2)
+	{
+		lout(stdout,"%s:%ld [%s]-%s@%s():[%d] :- %s",timestr,millisec,logtype,filename,funname,lineno,msg);
+	}
+	// freed allocated msg
+	free(msg);
+	msg = NULL;
+}
+
 _uchar_t getLogLevel()
 {
 	return g_log_setting.Level.value;
 }
+
+
 
 void InitLogger(_uchar_t level,_uchar_t devmode,char* filepath,_uchar_t stdpath)
 {
@@ -50,7 +110,7 @@ void InitLogger(_uchar_t level,_uchar_t devmode,char* filepath,_uchar_t stdpath)
 	}
 	else
 	{
-		g_File = freopen("/dev/tty","a",stderr);
+		g_File = fopen("/dev/tty","w");
 	}
 }
 
@@ -61,7 +121,26 @@ int main(int argc, char **argv)
 
 	LOG(LEVEL1,"Hello");
 
-	LOGL(LEVEL1,"Hello World Enter to new World");
+	g_log_setting.devmode = 2;
+
+	SMLOG(LEVEL1,("Hello World Enter to new World\n"));
+
+	LOGL(LEVEL1,("Log lvl 1 Test\n"));
+
+	LOGL(LEVEL_ERR,("Log lvl Err Test\n"));
+
+	LOGL(LEVEL_INVALID,("Log lvl invalid Test\n"));
+
+	LOGV(LEVEL_ERR,"Log V lvl Err Test\n");
+
+	// Now test Initilogger functionality
+	InitLogger(LEVEL1|LEVEL2|LEVEL3,1,(char *)"/tmp/core.txt",LOG_TO_STD_FILE);
+
+	LOGV(LEVEL3,"Init core.txt Log Actual 3 lvl Test\n");
+
+	InitLogger(LEVEL1|LEVEL2|LEVEL3,1,(char *)"",0);
+
+	LOGV(LEVEL2,"Init tty Log Actual 2 lvl Test\n");
 
 	return 0;
 }
--- logger.h
+++ logger.h
@@ -23,11 +23,19 @@ extern "C" {
 
 
 typedef unsigned char _uchar_t;
-#define MSGOUT print
-#define MSGFAIL "Failed"
+#define V_TEST_MSGOUT vprint
+#define LOGGER_MSG_FAILUE "Internal Logger Error"
 #define LOG_TO_STD_FILE '0'
+#define MAX_EXP_SIZE 512
+#define MAX_TIME_SIZE 64
 
-int print(const char* format,...);
+int vprint(const char* format,...);
+
+char* varargstostring(const char* format,...);
+
+#define TO_VSTRING(x) varargstostring(x)
+#define STRINGIFY varargstostring
+#define TO_STRING(x) #x
 
 typedef enum log_level
 {
@@ -55,6 +63,8 @@ typedef struct _log_options
 	_uchar_t devmode;
 }logsetting;
 
+extern void devlogprint(const char* filename,const char* funname,int lineno,const char* logtype,char* msg);
+
 extern _uchar_t getLogLevel();
 
 extern void InitLogger(_uchar_t level,_uchar_t devmode,char* filepath,_uchar_t stdpath = 0);
@@ -62,11 +72,24 @@ extern void InitLogger(_uchar_t level,_uchar_t devmode,char* filepath,_uchar_t s
 extern FILE* g_File;
 
 #define LOG(level ,printf_exp) \
-		(level ? MSGOUT("%s \n",printf_exp) : MSGOUT("%s \n",MSGFAIL))
+		(level ? V_TEST_MSGOUT("%s \n",printf_exp) : V_TEST_MSGOUT("%s \n",LOGGER_MSG_FAILUE))
+
+//http://stackoverflow.com/questions/597078/file-line-and-function-usage-in-c
+//http://stackoverflow.com/questions/9229601/what-is-in-c-code?rq=1
+
+#define SMLOG(level ,printf_exp) \
+		if(getLogLevel()&level) \
+		devlogprint(__FILE__,__func__,__LINE__,TO_STRING(level),STRINGIFY printf_exp); \
+		else	\
+		V_TEST_MSGOUT("%s \n",LOGGER_MSG_FAILUE); \
+
+#define LOGL(level,printf_exp) \
+		((getLogLevel()&level)? devlogprint(__FILE__, __func__,__LINE__,TO_STRING(level), STRINGIFY printf_exp), 1:-!!0)
+
+#define LOGV(level,printf_exp) \
+		((getLogLevel()&level)? devlogprint(__FILE__, __func__,__LINE__,TO_STRING(level), TO_VSTRING(printf_exp)), 1:-!!0)
 
 
-#define LOGL(level ,printf_exp) \
-		((getLogLevel() & LEVEL1) ? MSGOUT("%s \n",printf_exp) : MSGOUT("%s \n",MSGFAIL))
 
 
 #ifdef __cplusplus
